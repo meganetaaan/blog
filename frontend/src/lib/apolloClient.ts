0a5fbde2d34ae144dac9eb650c6c0bee8ed1697f
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-02-25 02:57:03 +0900

    Add debug settings

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -1,23 +1,35 @@
 import { useMemo } from "react";
-import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 import { concatPagination } from "@apollo/client/utilities";
-import merge from "deepmerge"
-import isEqual from "lodash/isEqual"
+import merge from "deepmerge";
+import isEqual from "lodash/isEqual";
+import { onError } from "apollo-link-error";
+import { link } from "fs";
 
-export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__"
+export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__";
 
 let apolloClient: ApolloClient<NormalizedCacheObject>;
-const DEFAULT_GRAPHQL_ENDPOINT_URI = 'http://localhost:1337/graphql'
+const DEFAULT_GRAPHQL_ENDPOINT_URI = "http://localhost:1337/graphql";
 
 function createApolloClient() {
-  const uri = process.env.GRAPHQL_ENDPOINT_URI || process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URI || DEFAULT_GRAPHQL_ENDPOINT_URI
+  const uri =
+    process.env.GRAPHQL_ENDPOINT_URI || process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URI || DEFAULT_GRAPHQL_ENDPOINT_URI;
+  const httpLink = new HttpLink({
+    uri,
+    credentials: "same-origin"
+  });
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+  const link = ApolloLink.from([errorLink, httpLink]);
 
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
-    link: new HttpLink({
-      uri,
-      credentials: "same-origin"
-    }),
+    link: link,
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
@@ -27,7 +39,7 @@ function createApolloClient() {
         }
       }
     })
-  })
+  });
 }
 
 export function initializeApollo(initialState = null) {
@@ -37,40 +49,38 @@ export function initializeApollo(initialState = null) {
   // gets hydrated here
   if (initialState != null) {
     // Get existing cache, loaded during client side data fetching
-    const existingCache = _apolloClient.extract()
+    const existingCache = _apolloClient.extract();
 
     // Merge the existing cache into data passed from getStaticProps/getServerSideProps
     const data = merge(initialState ?? {}, existingCache, {
       // combine arrays using object equality (like in sets)
       arrayMerge: (destinationArray, sourceArray) => [
         ...sourceArray,
-        ...destinationArray.filter((d) =>
-          sourceArray.every((s) => !isEqual(d, s))
-        ),
-      ],
-    })
+        ...destinationArray.filter((d) => sourceArray.every((s) => !isEqual(d, s)))
+      ]
+    });
 
     // Restore the cache with the merged data
-    _apolloClient.cache.restore(data)
+    _apolloClient.cache.restore(data);
   }
   // For SSG and SSR always create a new Apollo Client
-  if (typeof window === 'undefined') return _apolloClient
+  if (typeof window === "undefined") return _apolloClient;
   // Create the Apollo Client once in the client
-  if (!apolloClient) apolloClient = _apolloClient
+  if (!apolloClient) apolloClient = _apolloClient;
 
-  return _apolloClient
+  return _apolloClient;
 }
 
 export function addApolloState(client: ApolloClient<unknown>, pageProps: any) {
   if (pageProps?.props) {
-    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
   }
 
-  return pageProps
+  return pageProps;
 }
 
 export function useApollo(pageProps: any) {
-  const state = pageProps[APOLLO_STATE_PROP_NAME]
-  const store = useMemo(() => initializeApollo(state), [state])
-  return store
+  const state = pageProps[APOLLO_STATE_PROP_NAME];
+  const store = useMemo(() => initializeApollo(state), [state]);
+  return store;
 }
