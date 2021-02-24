176a70b904ffa91065ac6ab9b1ddfd36c8173632
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-02-25 01:21:44 +0900

    Make responsive

--- frontend/components/elements/article-card.tsx
+++ frontend/components/elements/article-card.tsx
@@ -1,29 +1,72 @@
-import { BoxProps, Flex, Heading, HStack, Image, LinkBox, LinkOverlay, Tag, Text, VStack } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  BoxProps,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Spacer,
+  Stack,
+  Tag,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import Link from "next/link";
 import React, { FC } from "react";
 import { Article } from "../../src/generated/graphql";
 import { getStrapiMedia } from "../../src/lib/media";
 
-interface ArticleCardProps extends Pick<Article, 'slug' | 'publishedAt' | 'title' | 'description' | 'tags'>, Omit<BoxProps, 'title'> {
-  imageUrl: string
+interface ArticleCardProps
+  extends Pick<Article, "slug" | "publishedAt" | "title" | "description" | "tags">,
+    Omit<BoxProps, "title"> {
+  imageUrl?: string;
 }
 const ArticleCard: FC<ArticleCardProps> = ({ slug, title, description, tags, imageUrl }: ArticleCardProps) => {
-  return <LinkBox as="article" transitionProperty="all" transition="ease-in" transitionDuration="100ms" shadow="sm" borderWidth="1px" overflow="hidden"
-    rounded="md" _hover={{
-    boxShadow: "base",
-    transform: "translateY(-2px) scale(1.02)"
-  }} >
-    <Flex direction="column">
-      <Image w="full" objectFit="cover" src={getStrapiMedia(imageUrl) ?? ""} />
-      <VStack p={4}>
-        <Heading color="gray.800" fontSize="xl" fontWeight="bold">
-          <LinkOverlay href={slug}>{title}</LinkOverlay>
-        </Heading>
-        <HStack w="full">
-          {tags?.map(m => (<Tag size="sm">{m?.name}</Tag>))}
-        </HStack>
-        <Text color="gray.800">{description}</Text>
-      </VStack>
-    </Flex>
-  </LinkBox>
-}
+  return (
+    <Link href={slug}>
+      <Box
+        as="article"
+        transitionProperty="all"
+        transition="ease-in"
+        transitionDuration="100ms"
+        shadow="sm"
+        borderWidth="1px"
+        overflow="hidden"
+        rounded="md"
+        _hover={{
+          cursor: "pointer",
+          boxShadow: "base",
+          transform: "translateY(-2px) scale(1.02)"
+        }}
+      >
+        <Stack direction={["row", null, "column"]}>
+          <VStack p={4} align="left">
+            <Heading color="gray.800" fontSize="xl" fontWeight="bold">
+              {title}
+            </Heading>
+            {tags != null && tags.length > 0 && (
+              <HStack w="full">
+                {tags?.map((m) => (
+                  <Tag key={m?.id} as="a" href="#" size="sm">
+                    {m?.name}
+                  </Tag>
+                ))}
+              </HStack>
+            )}
+            <Text color="gray.800" isTruncated noOfLines={3}>
+              {description}
+            </Text>
+          </VStack>
+          <Spacer></Spacer>
+          <AspectRatio minW={20} w={["25%", null, "full"]} ratio={[1, null, 16 / 9]}>
+            <Image w="full" objectFit="cover" src={getStrapiMedia(imageUrl) ?? ""} />
+          </AspectRatio>
+        </Stack>
+      </Box>
+    </Link>
+  );
+};
 export default ArticleCard;
--- frontend/components/elements/articles.tsx
+++ frontend/components/elements/articles.tsx
@@ -4,18 +4,25 @@ import { Article } from "../../src/generated/graphql";
 import ArticleCard from "./article-card";
 
 interface Props {
-  articles: Pick<Article, 'id' | 'slug' | 'publishedAt' | 'title' | 'description' | 'image' | 'tags'>[]
+  articles: Pick<Article, "id" | "slug" | "publishedAt" | "title" | "description" | "image" | "tags">[];
 }
 const ArticleList: FC<Props> = ({ articles }) => (
   <Container maxW="6xl">
-    <SimpleGrid columns={[1, 2, 3]} gridGap={4}>
-      {articles.map(a => (
+    <SimpleGrid columns={[1, null, 3]} gridGap={4}>
+      {articles.map((a) => (
         <Box key={a.id}>
-          <ArticleCard slug={a.slug} title={a.title} description={a.description} tags={a.tags} imageUrl={a.image?.url} />
+          <ArticleCard
+            slug={a.slug}
+            title={a.title}
+            description={a.description}
+            tags={a.tags}
+            imageUrl={a.image?.url}
+            publishedAt={a.publishedAt}
+          />
         </Box>
       ))}
     </SimpleGrid>
   </Container>
-)
+);
 
-export default ArticleList
+export default ArticleList;
