8974cd04417507064ec63542066ed8eafc66eab5
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-03-12 03:33:30 +0900

    Fix glitch on loading

--- frontend/components/elements/progressive-image.tsx
+++ frontend/components/elements/progressive-image.tsx
@@ -1,6 +1,6 @@
 import { BoxProps } from "@chakra-ui/layout"
-import { Image as ChakraImage } from "@chakra-ui/react"
-import { FC, useEffect, useState } from "react"
+import { Fade, Image as ChakraImage } from "@chakra-ui/react"
+import React, { FC, Fragment, useEffect, useState } from "react"
 import { Article } from "../../src/generated/graphql"
 import { getStrapiMedia } from "../../src/lib/media"
 
@@ -19,10 +19,12 @@ const ProgressiveImage: FC<ProgressiveImageProps> = ({ image, ...props }) => {
         }
     })
 
-    return !isLoaded ? (<ChakraImage alt={image?.alternativeText || ""} position="absolute" top={0} left={0} objectFit="cover" w="full" h="full" src={getStrapiMedia(thumb.url)} style={{
-        filter: "blur(4px)"
-    }}></ChakraImage>) :
-        <ChakraImage alt={image?.alternativeText || ""} objectFit="cover" src={getStrapiMedia(image?.url)}></ChakraImage>
+    return <Fragment>
+        <ChakraImage alt={image?.alternativeText || ""} objectFit="cover" w="full" h="full" src={getStrapiMedia(thumb.url)} style={{ filter: "blur(4px)"}}></ChakraImage>
+        <Fade in={isLoaded}>
+            <ChakraImage alt={image?.alternativeText || ""} objectFit="cover" w="full" h="full" src={getStrapiMedia(image?.url)}></ChakraImage>
+        </Fade>
+    </Fragment>
 }
 
 export default ProgressiveImage
