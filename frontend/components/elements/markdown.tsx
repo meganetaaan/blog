3d8992f99bf3672b30bba76fffc9c5be1f31062a
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-03-22 02:07:47 +0900

    Fix styles of list and header

--- frontend/components/elements/markdown.tsx
+++ frontend/components/elements/markdown.tsx
@@ -1,17 +1,17 @@
 import { LinkIcon } from "@chakra-ui/icons";
-import { Box, BoxProps, Heading, HStack, Image, Link, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, BoxProps, Divider, Heading, HStack, Image, Link, OrderedList, Text, UnorderedList, useBreakpointValue } from "@chakra-ui/react";
 import ChakraUIRenderer, { defaults } from "chakra-ui-markdown-renderer";
-import React from "react";
+import React, { Fragment } from "react";
 import ReactMarkdown from "react-markdown";
-import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
-import defaultVscDarkPlus from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
+import { PrismAsync as SyntaxHighlighter } from "react-syntax-highlighter";
+import defaultVscDarkPlus from "react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus";
 import { getStrapiMedia } from "../../src/lib/util";
 
 interface MarkdownProps extends BoxProps {
   content: string;
 }
 
-function getCoreProps(props: any) {
+function getCoreProps(props: any): any {
   return props["data-sourcepos"] ? { "data-sourcepos": props["data-sourcepos"] } : {};
 }
 
@@ -25,6 +25,25 @@ const newTheme = {
       </Text>
     );
   },
+  list: (props: any) => {
+    const { start, ordered, children, depth } = props;
+    const attrs = getCoreProps(props);
+    if (start !== null && start !== 1 && start !== undefined) {
+      attrs.start = start.toString();
+    }
+    let Element = UnorderedList;
+    let styleType = "disc";
+    if (ordered) {
+      Element = OrderedList;
+      styleType = "decimal";
+    }
+    if (depth === 1) styleType = "circle";
+    return (
+      <Element fontSize={["sm", null, "md"]} spacing={0} as={ordered ? "ol" : "ul"} styleType={styleType} pl={4} {...attrs}>
+        {children}
+      </Element>
+    );
+  },
   text: (props: any) => {
     const { children } = props;
     return (
@@ -33,64 +52,69 @@ const newTheme = {
       </Text>
     );
   },
-  link: (props: any) => (
-    <Link color="teal.500" target="_blank" {...props}></Link>
-  ),
-  img: ({src, ...props}: any) => (
-    <Image py={4} src={getStrapiMedia(src)} {...props}></Image>
-  ),
+  link: (props: any) => <Link color="teal.500" target="_blank" {...props}></Link>,
+  img: ({ src, ...props }: any) => <Image py={4} src={getStrapiMedia(src)} {...props}></Image>,
   code: ({ language, value }: any) => (
-    <SyntaxHighlighter style={defaultVscDarkPlus} showLineNumbers={true} language={language} children={value}></SyntaxHighlighter>
+    <SyntaxHighlighter
+      style={defaultVscDarkPlus}
+      showLineNumbers={true}
+      language={language}
+      children={value}
+    ></SyntaxHighlighter>
   ),
   heading: (props: any) => {
-    const offset = useBreakpointValue({
-      base: 0,
-      lg: 1
-    }) ?? 0;
-    let firstChildText = ""
-    let node = props.node
+    const offset =
+      useBreakpointValue({
+        base: 0,
+        lg: 1
+      }) ?? 0;
+    let firstChildText = "";
+    let node = props.node;
     while (node.children != null) {
-      firstChildText = node.children[0].value
-      node = node.children[0]
+      firstChildText = node.children[0].value;
+      node = node.children[0];
     }
     const { level, children }: { level: 1 | 2 | 3 | 4 | 5 | 6; children: any } = props;
-    const lv = Math.max(0, level - offset)
-    const sizes = ["2xl", "xl", "lg", "md", "base", "sm"];
+    const lv = Math.max(0, level - offset);
+    const sizes = ["2xl", "xl", "lg", "md", "base", "sm", "sm"];
     return (
-      <HStack>
-        <HStack role="group">
-          <Link href={`#${firstChildText}`} w="auto">
-            <Heading
-              id={firstChildText}
-              my={4}
-              as={`h${level}` as "h1"}
-              size={sizes[Number(`${lv}`)]}
-              {...getCoreProps(props)}
-            >
-              {children}
-            </Heading>
-          </Link>
-          <LinkIcon
-            visibility="hidden"
-            display="inline"
-            _groupHover={{
-              visibility: "visible"
-            }}
-          ></LinkIcon>
+      <Fragment>
+        <HStack>
+          <HStack role="group">
+            <Link href={`#${firstChildText}`} w="auto">
+              <Heading
+                id={firstChildText}
+                my={4}
+                as={`h${level}` as "h1"}
+                size={sizes[Number(`${lv}`)]}
+                {...getCoreProps(props)}
+              >
+                {children}
+              </Heading>
+            </Link>
+            <LinkIcon
+              visibility="hidden"
+              display="inline"
+              _groupHover={{
+                visibility: "visible"
+              }}
+            ></LinkIcon>
+          </HStack>
         </HStack>
-      </HStack>
+        {level === 2 && <Divider borderColor="teal.500" mt={-6} mb={4}></Divider>}
+      </Fragment>
     );
   }
 };
 
-const theme = ChakraUIRenderer(newTheme)
-theme.image = theme.img
+const theme = ChakraUIRenderer(newTheme);
+theme.image = theme.img;
 const Markdown = ({ content, ...props }: MarkdownProps) => (
   <Box {...props} color="gray.800">
     <ReactMarkdown renderers={theme} escapeHtml={false}>
       {content}
     </ReactMarkdown>
   </Box>
-)
+);
 
 export default Markdown;
